befca8689e4d93b357a3e26f4e221281a87b52ff
Hukumitu Youhei <hukumitu.youhei@gmail.com>
2021-04-02 19:15:23 +0900

    fix index typ when typ is nil

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ long_name = "Target Myself"
 short_name = "target-myself" -- Determines the name of your addon's file.
 for_module = "tome"
 version = {1,7,2}
-addon_version = {0,1,0}
+addon_version = {0,1,1}
 -- should load after modest-auto-accept(99)
 weight = 100 -- The lower this value, the sooner your addon will load compared to other addons.
 author = {'hukumitu.youhei@gmail.com'}
--- superload/mod/class/Player.lua
+++ superload/mod/class/Player.lua
@@ -3,7 +3,7 @@ local _M = loadPrevious(...)
 local super_getTarget = _M.getTarget
 function _M:getTarget(typ)
 	local target_myself = game.player.addon.target_myself or {}
-	if typ.talent and target_myself[typ.talent.id] then
+	if typ and typ.talent and target_myself[typ.talent.id] then
 		return game.player.x, game.player.y, game.player
 	else
 		return super_getTarget(self, typ)
