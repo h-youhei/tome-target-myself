f305f35521d60cdaba4a0025141444e2d01d92b6
Hukumitu Youhei <hukumitu.youhei@gmail.com>
2021-05-02 10:13:29 +0900

    fix error when we try to target without setting up

--- superload/mod/class/Player.lua
+++ superload/mod/class/Player.lua
@@ -2,6 +2,7 @@ local _M = loadPrevious(...)
 
 local super_getTarget = _M.getTarget
 function _M:getTarget(typ)
+	game.player.addon = game.player.addon or {}
 	local target_myself = game.player.addon.target_myself or {}
 	if typ and typ.talent and target_myself[typ.talent.id] then
 		return game.player.x, game.player.y, game.player
